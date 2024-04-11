058c019fc02cd88445c50f35e0ebe92ad64c1257
Vladimir Goryachev <gorych15200@yandex.ru>
2024-04-12 02:36:24 +0300

    1. Поменял названия методов для создания задач 2. Отделил методы пустой строкой 3.Изменил название булевой переменной в медоте updateStatus

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -17,38 +17,47 @@ public class TaskManager {
     private int generationId() {
         return ++id;
     }
-    public Task creationTask(Task task) {
+    public Task createTask(Task task) {
         task.setId(generationId());
         tasks.put(task.getId(), task);
         return task;
     }
+
     public Task updateTask(Task task) {
         tasks.put(task.getId(), task);
         return task;
     }
+
     public ArrayList<Task> getAllTask() {
         return new ArrayList<>(tasks.values());
     }
+
     public void removeAllTask() {
         tasks.clear();
     }
+
     public Task getTask(int id) {
         return tasks.get(id);
     }
+
     public void removeTask(int id) {
         tasks.remove(id);
     }
-    public EpicTask creationEpic(EpicTask epicTask) {
+
+    public EpicTask createEpic(EpicTask epicTask) {
         epicTask.setId(generationId());
         epics.put(epicTask.getId(), epicTask);
         return epicTask;
     }
+
     public EpicTask getEpicTask(int id) {
         return epics.get(id);
     }
+
     public ArrayList<SubTask> getSubTaskEpicTask(EpicTask epicTask) {
         return new ArrayList<SubTask>(epicTask.getSubTasks());
     }
+
     public void removeEpicTask(int id) {
         EpicTask epicTaskCopy = epics.remove(id);
         ArrayList<SubTask> subTask = epicTaskCopy.getSubTasks();
@@ -57,22 +66,27 @@ public class TaskManager {
         }
         subTask.clear();
     }
+
     public ArrayList<EpicTask> getAllEpicTask() {
         return new ArrayList<>(epics.values());
     }
+
     public void removeAllEpicTask() {
         removeAllSubTask();
         epics.clear();
     }
+
     public void updateEpicTask(EpicTask epicTask) {
             EpicTask epicTaskCopy = epics.get(epicTask.getId());
             epicTaskCopy.setName(epicTask.getName());
             epicTaskCopy.setDescription(epicTask.getDescription());
     }
+
     public ArrayList<SubTask> getAllSubTaskEpic(EpicTask epicTask) {
         return epicTask.getSubTasks();
     }
-    public SubTask creationSubTask(SubTask subTask) {
+
+    public SubTask createSubTask(SubTask subTask) {
         EpicTask epic = epics.get(subTask.getEpic().getId());
         epic.addSubTask(subTask);
         subTask.setId(generationId());
@@ -80,12 +94,15 @@ public class TaskManager {
         subTasks.put(subTask.getId(), subTask);
         return subTask;
     }
+
     public SubTask getSubTask(int id) {
         return subTasks.get(id);
     }
+
     public ArrayList<SubTask> getAllSubTask() {
         return new ArrayList<>(subTasks.values());
     }
+
     public void removeSubTask(int id) {
         SubTask removeSubTask = subTasks.remove(id);
         EpicTask epicTask = removeSubTask.getEpic();
@@ -93,6 +110,7 @@ public class TaskManager {
         updateStatus(epicTask);
 
     }
+
     public void removeAllSubTask() {
         subTasks.clear();
         Collection<EpicTask> epicTasks = epics.values();
@@ -101,21 +119,23 @@ public class TaskManager {
             updateStatus(epicTaskCopy);
         }
     }
+
     public void updateSubTask(SubTask subTask) {
         if (subTasks.containsKey(subTask.getId())) {
             updateStatus(subTask.getEpic());
             subTasks.put(subTask.getId(), subTask);
         }
     }
+
     private void updateStatus(EpicTask epicTask) {
-        boolean isStatus = true;
+        boolean isStatusDone = true;
         for (SubTask copyEpicSubTask : epicTask.getSubTasks()) {
             if (copyEpicSubTask.getStatus() != Status.DONE) {
-                isStatus = false;
+                isStatusDone = false;
                 break;
             }
         }
-        if (!isStatus) {
+        if (!isStatusDone) {
             for (SubTask copyEpicSubTask : epicTask.getSubTasks()) {
                 if (copyEpicSubTask.getStatus() == Status.IN_PROGRESS) {
                     epicTask.setStatus(Status.IN_PROGRESS);
